2c6cdb96047d0c62b7f85e6769d71ef41f421401
Feliaguirre <fely_s.a@hotmail.com>
2023-11-07 16:12:16 -0300

    back end terminado

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require("./src/db.js");
+const { conn, Country } = require("./src/db.js");
 const {
   saveCountriesApiToDb,
 } = require("./src/controllers/saveCountriesDb.js");
@@ -8,8 +8,15 @@ const PORT = 3001;
 
 conn
   .sync({ alter: true })
-  .then(() => {
-    // await saveCountriesApiToDb();
+  .then(async () => {
+    const countryCount = await Country.count();
+    if (countryCount === 0) {
+      await saveCountriesApiToDb();
+      console.log("Countries saved");
+    } else {
+      console.log("Countries already in db, skipping api call");
+    }
+
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
--- cr-pi-countries-main/server/src/server.js
+++ cr-pi-countries-main/server/src/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
-const { saveCountriesApiToDb } = require("./controllers/saveCountriesDb");
 
 const server = express();
 
@@ -11,7 +10,5 @@ server.use(express.json());
 server.use(cors());
 
 server.use(router);
-
-// saveCountriesApiToDb();
 
 module.exports = server;
